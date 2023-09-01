32ee0b4e9f3694a1f3cbb14b1d86b39b50aafead
ben140758 <57662051+ben140758@users.noreply.github.com>
2023-09-01 17:08:56 +0100

    Create config.py

---
+++ Backend/config.py
@@ -0,0 +1,8 @@
+class DefaultConfig:
+    MYSQL_DATABASE_USER = 'benjamin.clough'
+    MYSQL_DATABASE_PASSWORD = 'DATABASE PASSWORD'
+    MYSQL_DATABASE_DB = 'benjaminclough_COM6009M'
+    MYSQL_DATABASE_HOST = 'ysjcs.net'
+    USER_SECRET_KEY = 'UNMWCynYxYqvt-HrQpjPHYcEHspb1iUnLjg4vSjG1VQ'
+    ADMIN_SECRET_KEY = 'TzfuRTm0WK0FCDOh2apAY47qHgHEE-mvJH8p9ga0A-A'
+    REFRESH_KEY = 'hwgPAvSgeLedd3Y_ufgCFljHBJJz6rg5eTypbLb6KvA'
